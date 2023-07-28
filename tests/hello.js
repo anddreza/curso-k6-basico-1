d595ead6b15127c2f067ce8163dcc31c19342ac0
Andreza <andreza@multiplier.com.br>
2023-07-28 15:45:28 -0300

    feat: primeiros testes

--- tests/hello.js
+++ tests/hello.js
@@ -1,7 +1,20 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
 
 export default function () {
-  http.get('https://localhost:3333');
-  sleep(1);
+  const res = http.get('http://localhost:3333');
+  check(res, {
+    'status should be 200': (r) => r.status === 200
+     
+ })
+
+   sleep(1);
+    
 }
