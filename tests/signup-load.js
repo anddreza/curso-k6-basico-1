fae332d836f1c81c22b9b6bf6871b3e29effe1c1
Andreza <andreza@multiplier.com.br>
2023-07-28 15:46:29 -0300

    feat: signup, inicio da aplicação

--- tests/signup-load.js
+++ tests/signup-load.js
@@ -3,8 +3,11 @@ import { sleep, check } from 'k6';
 import uuid from './libs/uuid.js';
 
 export const options = {
-  vus: 10,
-  duration: '30s',
+  stages: [
+    { duration: '1m', target: 100},
+    { duration: '2m', target: 100},
+    { duration: '1m', target: 0}
+  ],
   thresholds: {
     http_req_duration: ['p(95)<2000'], // 95% das requisiçoes devem responder em ate 2s
     http_req_failed: ['rate<0.01'] // 1% das requisições podem ocorrer erro 
@@ -28,8 +31,7 @@ export default function () {
     }
   }
   const res = http.post(url, payload, headers)
-  
-  console.log(res.body)
+
 
   check(res, {
     'status should be 201': (r) => r.status === 201     
